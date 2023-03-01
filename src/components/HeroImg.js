16bc784edb343743db9b8570f2e7b3086ecce659
Terrance Smith <terrancesmith@Laquishas-MacBook-Air.local>
2023-03-01 17:33:49 -0500

    updated projects button

--- src/components/HeroImg.js
+++ src/components/HeroImg.js
@@ -14,7 +14,7 @@ const HeroImg = () => {
         <p> Hi, I am Terrance. </p>
         <h1> Web Developer </h1>
         <div>
-            <Link to='/projects' className='btn'> Projects</Link>
+            <Link to='/project' className='btn'> Projects</Link>
             <Link to='/contact' className='btn btn-light'> Contact</Link>
         </div>
     </div>
